bb244221d708df6e799e78b343d896b4d1bb2ac4
Fred <fred17@mail.ru>
2022-08-21 22:49:23 +0300

    added lesson_5

--- script.js
+++ script.js
@@ -1,83 +1,21 @@
-const goods = [
-    { img: 'img/sneakers.jpg', title: 'Кеды' },
-    { img: 'img/socks.jpg', title: 'Носки', price: 50 },
-    { img: 'img/jacket.jpg', title: 'Куртка', price: 350 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 }
-];
-
 const BASE = ' https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 const GOODS = '/catalogData.json';
 
-
-window.addEventListener('load', () => {
-    const goodsList = new GoodsList();
-    goodsList.fetchGoods()
-        .then(() => goodsList.render());
-})
-
-function service(url) {
-    return new Promise(function (resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        const loadHandler = () => {
-            resolve(JSON.parse(xhr.response))
+var app = new Vue({
+    el: '#app',
+    data: {
+        searchLine: '',
+        isVisibleCart: false,
+        goods: [],
+    },
+    methods: {
+        changeStateCart: function () {
+            this.isVisibleCart = this.isVisibleCart ? false : true;
         }
-        xhr.onload = loadHandler;
-        xhr.send();
-    })
-}
-
-class GoodsItem {
-    constructor({ img = "img/no_photo.jpg", product_name = " ", price = 0 }) {
-        this.img = img;
-        this.title = product_name;
-        this.price = price;
+      },
+    mounted() {
+        fetch(`${BASE}${GOODS}`)
+            .then(res => res.json())
+            .then(data => this.goods = data)
     }
-    render() {
-        return `
-        <div class="goods-item">
-            <img src=${this.img}>
-            <h3>${this.title}</h3>
-            <p>${this.price} р.</p>
-            <button>Добвить</button>
-        </div>
-    `;
-    }
-}
-
-class GoodsList {
-    goods = [];
-    fetchGoods() {
-        return service(`${BASE}${GOODS}`)
-            .then(res => {
-                this.goods = res;
-            })
-    }
-    calcGoods() {
-        return this.goods.reduce((a, { price }) => price ? a + price : a + 0, 0);
-    }
-    render() {
-        const goodsList = this.goods.map(item => {
-            const goodsItem = new GoodsItem(item);
-            return goodsItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
-    }
-}
+})
